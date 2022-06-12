fafc982a4294e3ab2789112c7ce633abaea805e4
Chloe <limkeyee@gmail.com>
2022-06-12 16:39:58 +0800

    first flash

--- app/src/main/java/com/example/jasiriheart/MainActivity.kt
+++ app/src/main/java/com/example/jasiriheart/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.jasiriheart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.jasiriheart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val settingsFrag = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        setCurrentFragment(settingsFrag)
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        val fragMgr = supportFragmentManager
+
+        fragMgr.beginTransaction().apply{
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+    }
+
+    private fun bottomNavSelect() {
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+//                R.id.study -> setCurrentFragment(firstFragment)
+//                R.id.home -> setCurrentFragment(secondFragment)
+                R.id.settings -> setCurrentFragment(settingsFrag)
+
+            }
+            true
+        }
+    }
+
 }
+
