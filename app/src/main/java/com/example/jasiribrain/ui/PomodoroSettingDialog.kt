71889ac901dfcc180031dfe08b31817f36bb0f38
Chloe <limkeyee@gmail.com>
2022-12-14 16:43:36 +0800

    added dialog for pomodoro settings UI

--- app/src/main/java/com/example/jasiribrain/ui/PomodoroSettingDialog.kt
+++ app/src/main/java/com/example/jasiribrain/ui/PomodoroSettingDialog.kt
@@ -1,13 +1,27 @@
 package com.example.jasiribrain.ui
 
-import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jasiribrain.databinding.DialogPomodoroSettingBinding
 
-class PomodoroSettingDialog: DialogFragment() {
+class PomodoroSettingDialog: Fragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+    private var _binding: DialogPomodoroSettingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogPomodoroSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
